73a308a3a016dfbbe2821bfe6992ba37276e9360
opank <opankov052@gmail.com>
2026-01-29 22:37:11 +0500

    сделал отображение результата + сообщение об ошибке на клиентской части, добавил модели для отображения результата, изменил логику в сервисе чтобы он правильно считал выражение

--- src/main/java/com/example/WarehouseAccountingSystem/controller/WarehouseAccountingSystemController.java
+++ src/main/java/com/example/WarehouseAccountingSystem/controller/WarehouseAccountingSystemController.java
@@ -1,23 +1,39 @@
 package com.example.WarehouseAccountingSystem.controller;
 
 import com.example.WarehouseAccountingSystem.dto.WarehouseAccountingSystemDTO;
+import com.example.WarehouseAccountingSystem.service.WarehouseAccountingSystemService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping
+@RequiredArgsConstructor
 public class WarehouseAccountingSystemController {
+    private final WarehouseAccountingSystemService SERVICE;
+
+    private Long result;
 
     @GetMapping("/home")
-    public String home() {
+    public String home(Model model) {
+        try {
+            model.addAttribute("result", result);
+            model.addAttribute("isLessZero", result < 0);
+        } catch (Exception e) {
+            System.err.println("ERROR: " + e.getMessage());
+        }
+
         return "index";
     }
 
     @PostMapping("/result")
     public String result(@Valid WarehouseAccountingSystemDTO dto) {
+        result = SERVICE.resultProductBalances(dto);
+
         return "redirect:/home";
     }
 }
--- src/main/java/com/example/WarehouseAccountingSystem/service/impl/WarehouseAccountingSystemServiceImpl.java
+++ src/main/java/com/example/WarehouseAccountingSystem/service/impl/WarehouseAccountingSystemServiceImpl.java
@@ -2,10 +2,12 @@ package com.example.WarehouseAccountingSystem.service.impl;
 
 import com.example.WarehouseAccountingSystem.dto.WarehouseAccountingSystemDTO;
 import com.example.WarehouseAccountingSystem.service.WarehouseAccountingSystemService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class WarehouseAccountingSystemServiceImpl implements WarehouseAccountingSystemService {
     @Override
     public Long resultProductBalances(WarehouseAccountingSystemDTO dto) {
-        return Long.parseLong(dto.getAllGoodsStock()) - Long.parseLong(dto.getComing()) - Long.parseLong(dto.getExpenditure());
+        return Long.parseLong(dto.getAllGoodsStock()) - Long.parseLong(dto.getExpenditure()) + Long.parseLong(dto.getComing());
     }
 }
