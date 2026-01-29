3b8debea8384a546d1e9000d5af37075af515116
opank <opankov052@gmail.com>
2026-01-29 21:31:53 +0500

    Initial commit

---
+++ src/main/java/com/example/WarehouseAccountingSystem/WarehouseAccountingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.WarehouseAccountingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseAccountingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseAccountingSystemApplication.class, args);
+	}
+
+}
